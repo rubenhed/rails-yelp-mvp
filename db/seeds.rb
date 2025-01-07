8df2b23a1b58b623c7f2f77ba9e919e8db567451
Ruben HedstrÃom <ruben.hedstrom@hotmail.com>
2025-01-07 21:05:20 +0900

    made controllers and routes, todo: all actions, starting w rest index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+ch = Restaurant.new(name: 'chinese-food', category: 'chinese', address: 'chinastreet')
+it = Restaurant.new(name: 'italian-food', category: 'italian', address: 'italiastreet')
+jp = Restaurant.new(name: 'japanese-food', category: 'japanese', address: 'japanstreet')
+fr = Restaurant.new(name: 'french-food', category: 'french', address: 'chinastreet')
+be = Restaurant.new(name: 'belgian-food', category: 'belgian', address: 'belgiumstreet')
+
+[ch, it, jp, fr, be].each do |rest|
+  puts "creating #{rest.name} if valid: #{rest.valid?}"
+  rest.save!
+end
