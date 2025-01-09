b9442221dae3a7848ef5f5a86df52a5bff7a99d1
Ruben Hedstrom/ルーベン・ヘドストロム <ruben.hedstrom@gadelius.com>
2025-01-09 15:14:53 +0900

    dun with basic stuff, little design

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,25 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = @restaurant.reviews
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 end
